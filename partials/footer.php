26b5d4addc135b0edf12fddc17a95da559572940
Muhammad Abdullah <90593591+Abdullah359@users.noreply.github.com>
2023-11-10 01:39:57 -0800

    Add files via upload

---
+++ partials/footer.php
@@ -0,0 +1,6 @@
+<footer>
+            <p>
+                Copyright &copy; 2023 JavaJam Coffee Bar<br>
+                <a href="">feed123@jvjam.com</a>
+            </p>
+</footer>
---
+++ partials/navbar.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg ps-4 d-flex">
+    <div class="container-fluid">
+        <a class="navbar-brand text-white ms-3 f_family" href="#">Java Jam</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse justify-content-end pt-1" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link fs-4 fw-normal text-white me-3" href="home.php">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link fs-4 fw-normal text-white mx-3" href="menu.php">Menu</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fs-4 fw-normal text-white mx-3" href="music.php">Music</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle fs-4 fw-normal text-white ms-3" role="button" data-bs-toggle="dropdown" aria-expanded="false">More</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item disabled" aria-disabled="true">About Us</a></li>
+                        <li><a class="dropdown-item disabled" aria-disabled="true">Contact Us</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="LogIn_SignUp/logout.php">Log Out</a></li>
+                    </ul>
+                </li>
+                <!-- <li class="nav-item">
+                    <a class="nav-link fs-4 fw-normal text-white ms-3" href="LogIn_SignUp/logout.php">Log Out</a>
+                </li> -->
+            </ul>
+        </div>
+    </div>
+</nav>
